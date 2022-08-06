03dbe15a84f146c84890fcf65ad7695e6599b02a
Artgrechanj <artgrechanj@gmail.com>
2022-08-06 17:38:18 +0200

    start scripts

--- alpanometer.sh
+++ alpanometer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #screen -dmS ametr_test /opt/ametr/start_test_board.sh
-screen -dmS ametr_load /opt/ametr/start_load_video.sh
+screen -dmS ametr_http /opt/print_ruber/start_httpd.sh
 #screen -dmS ametr_scan /opt/ametr/start_qr_scan.sh
 
 exit 0
